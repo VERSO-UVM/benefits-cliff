19e094d97f473344f7b08776780571fef409f777
Fitz Koch <fw.j.keenan.koch@gmail.com>
2026-02-06 10:05:29 -0500

    added caveats and changed name to estimator

---
+++ info/overall_design.md
@@ -0,0 +1,18 @@
+# Order of Operations
+
+- add interactive visualizations for users
+- add static narratives for 'test cases'
+- maybe get in touch with building bright futures and public assets institute
+
+# Things to add
+
+## Methods (alongside sources)
+
+## How to use
+
+## Data Citations
+
+## Key Partners or Stakeholders
+
+- eg people who administer foodstamps, and so on
+- pitch it to them first?
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import MainInputForm from "./components/MainInputForm";
 import calculateBenefits from "./utils/benefitCalculators";
 import Results from "./components/BenefitsDisplay";
-import { Title, Center } from "@mantine/core";
+import { Title, Center, Alert, Stack } from "@mantine/core";
 
 import type {
   RawHouseholdData,
@@ -30,12 +30,25 @@ function App() {
   return (
     <div className="App">
       <Center>
-        <Title order={1}>Vermont Benefits Cliff Calculator</Title>
+        <Stack align="center">
+          <Title order={1}>Vermont Benefits Estimator</Title>
+
+          <MainInputForm onCalculate={handleCalculate} />
+          {result && processedData && (
+            <Results result={result} processedData={processedData} />
+          )}
+          <Alert color="red" variant="filled" title="Warning">
+            Caveat: the Vermont Benefits Estimator is intended as an initial
+            reference.
+            <br />
+            It is a work in progress and does not include all factors involved
+            in benefits estimation.
+            <br />
+            Always trust current state guidelines, policies, and conclusions
+            over the estimations you see here.
+          </Alert>
+        </Stack>
       </Center>
-      <MainInputForm onCalculate={handleCalculate} />
-      {result && processedData && (
-        <Results result={result} processedData={processedData} />
-      )}
     </div>
   );
 }
