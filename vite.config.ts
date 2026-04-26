041ef0d1ed4b5d167575723e75f29244f475ee6a
Fitz Koch <fw.j.keenan.koch@gmail.com>
2026-04-26 15:34:14 -0400

    attempt at embedding website in quarto

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   root: "app",
   plugins: [react()],
+  base: "/app/",
   resolve: {
     alias: {
       "/src": path.resolve(__dirname, "src"),
